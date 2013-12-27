a3ef3fe236ce30919091665300391d197d742bc4
Ricardo <ricardo.gon.tell@gmail.com>
2013-12-27 00:37:14 -0600

    Primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aws::Application.config.secret_key_base = '49641a166b2f635dd27a1df9fb0d2a2559de31f17d16da08734388b32e1bd996fc3d6227575a59f89f84eddc4adb5506866e82b8fe145a2d093d9081b154db18'
