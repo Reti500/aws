82da8f88d4ff98392cdd67ab95978f7dc34c80ef
Ricardo Gonzalez Tellez <ricardo@softco.mx>
2014-03-26 14:04:29 -0600

    CSRF verify

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -1,4 +1,7 @@
 class ProductosController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  
   before_action :set_producto, only: [:show, :edit, :update, :destroy]
 
   # GET /productos
