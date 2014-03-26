d6abf5ed8118eac7cb466229e4495f174b5ec333
Ricardo Gonzalez Tellez <ricardo@softco.mx>
2014-03-26 14:41:55 -0600

    Estable para subir imagenes, cambio de cubeta a newsimagenes

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -3,7 +3,7 @@ class Producto < ActiveRecord::Base
 
 	has_attached_file :imagen,
     :storage => :s3,
-    :bucket => 'pruebaimg',
+    :bucket => 'newsimagenes',
     :s3_credentials => {
       :access_key_id => 'AKIAI37NH6YABDJANX6A',
       :secret_access_key => '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',
